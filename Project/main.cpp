cf1deb38d5c782a2d4d9332d46764cdbebca72f4
ctrlayan <ayanahmedcr7@gmail.com>
2026-05-10 11:49:40 +0500

    main is updated

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,77 +1,109 @@
 #include <iostream>
+#include <iomanip>
+#include "Input.h"
 #include "Admin.h"
+#include "FileManager.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
+    cout << fixed << setprecision(2);
+
     Admin admin;
+
+    FileManager::loadRates();
+    FileManager::loadConsumers(admin.getConsumer(), admin.getCount());
+
     int choice;
 
-        cout << "-----SMART ELECTRICITY PORTAL-----" << endl;
-        cout << endl;
+    do
+    {
+        cout << "===== Smart Electricity Billing System ====="<<endl;
 
-    do {
-        cout << "1. Add Consumer" << endl;
-        cout << "2. Display All" << endl;
-        cout << "3. Search Consumer" <<endl;
-        cout << "4. Generate Bill" << endl;
-        cout << "5. Set Rates" << endl;
-        cout << "6. Exit" << endl;
+        cout << "1. Add Consumer"<<endl;
+        cout << "2. Display All Consumers"<<endl;
+        cout << "3. Search Consumer By ID"<<endl;
+        cout << "4. Generate Bill"<<endl;
+        cout << "5. Set Rates"<<endl;
+        cout << "6. Exit"<<endl;
 
-        cin >> choice;
+        choice = inputIntRange("Enter Choice: ", 1, 6);
 
-        switch(choice) {
+        switch (choice)
+        {
             case 1:
+            {
                 admin.addConsumer();
                 break;
+            }
             case 2:
+            {
                 admin.displayAll();
                 break;
+            }
             case 3:
             {
                 int id;
-                cout << "Enter ID to search: ";
-                cin >> id;
+                id = inputInt("Enter ID: ");
 
                 Consumer* c = admin.searchConsumerbyID(id);
 
-                if (c != NULL)
+                if (c == NULL)
                 {
-                  cout << c->Display();
+                    cout << "Consumer not found!"<<endl;
                 }
                 else
                 {
-                  cout << "Consumer not found!\n";
+                    cout << c->Display();
+                    cout << "Bill: "<< c->calculateBill()<< endl;
                 }
+
                 break;
             }
+
             case 4:
+            {
                 admin.generateBill();
                 break;
+            }
+
             case 5:
             {
                 float peak,offpeak,commercial,solar;
 
-                cout << "Enter Residential Peak Rate: ";
-                cin >> peak;
+                peak = inputFloat("Enter Residential Peak Rate: ");
+                offpeak = inputFloat("Enter Residential Off-Peak Rate: ");
+                commercial = inputFloat("Enter Commercial Rate: ");
+                solar = inputFloat("Enter Solar Rate: ");
 
-                cout << "Enter Residential Off-Peak Rate: ";
-                cin >> offpeak;
+                admin.setRates(peak , offpeak , commercial , solar);
+                FileManager::saveRates();
 
-                cout << "Enter Commercial Rate: ";
-                cin >> commercial;
+                cout << "Rates updated successfully!"<<endl;
 
-                cout << "Enter Solar Rate: ";
-                cin >> solar;
-
-                admin.setRates(peak,offpeak,commercial,solar);
                 break;
             }
-            default:
+
+            case 6:
+            {
+                FileManager::saveConsumers(admin.getConsumer() , admin.getCount());
+
+                FileManager::saveRates();
+
+                cout << "Data saved successfully!"<<endl;
+                cout << "Exiting program..."<<endl;
+
                 break;
+            }
+
+            default:
+            {
+                cout << "Invalid choice!"<<endl;
+            }
         }
 
-    } while(choice != 6);
+    } while (choice != 6);
 
     return 0;
 }
